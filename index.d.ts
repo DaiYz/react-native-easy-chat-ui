fdb51854924fffe8cba823492dd7ab6b1c2d4fce
杨俊宁 <1003719811@qq.com>
2019-08-05 17:06:54 +0800

    完成 index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -2,7 +2,7 @@ import ChatView from './app/chat/ChatView'
 import { ViewStyle } from 'react-native'
 import { PureComponent } from 'react'
 
-enum msgType {
+enum MsgType {
   text = 'text',
   image = 'image',
   voice = 'voice',
@@ -16,7 +16,7 @@ enum msgType {
   system = 'system',
 }
 
-export interface messagePorpsType {
+export interface MessagePorpsType {
   id: string
   /**
    * 单独消息内容对象
@@ -25,7 +25,7 @@ export interface messagePorpsType {
     /**
      * 消息类型
      */
-    type: msgType
+    type: MsgType
     /**
      * 消息内容
      */
@@ -54,14 +54,15 @@ export interface messagePorpsType {
 }
 
 export interface ChatScreenPropsType {
+  // 消息属性
   /**
    * 消息列表
    */
   messageList: {
     any: {
-      messages: messagePorpsType[]
+      messages: MessagePorpsType[]
       /**
-       * 当消息长度超出屏幕高度时为true否则为false
+       * 当消息长度超出屏幕高度时为true否则为false，用于历史消息功能
        * (请在在componentWillUnmount生命周期中或者删除消息的时候修改此值)
        */
       inverted: boolean
@@ -83,32 +84,340 @@ export interface ChatScreenPropsType {
   /**
    * 发送消息时的回调
    */
-  sendMessage: (type: msgType, content: string, isInverted: boolean) => void
+  sendMessage: (type: MsgType, content: string, isInverted: boolean) => void
   /**
    * 重发消息的回调
    */
-  reSendMessage: (type: msgType, content: string) => void
+  reSendMessage?: (type: MsgType, content: string) => void
   /**
    * 删除消息的回调
    */
-  delMessage: (indexs, isInverted: boolean) => void
+  delMessage?: (indexs, isInverted: boolean) => void
   /**
    * 头像样式
    */
-  avatarStyle: ViewStyle
+  avatarStyle?: ViewStyle
   /**
    * 聊天对象的ID
    */
-  chatId: string
+  chatId?: string
   /**
    * 与聊天人的关系
    */
-  chatType: 'friend'
+  chatType?: 'friend'
+  /**
+   * 点击消息的回调
+   */
+  onMessagePress?: (type: MsgType, index: number, content: string, message: MessagePorpsType) => void
+  /**
+   * 长按消息的回调(usePopView为false时候触发，默认显示一个弹出层)
+   */
+  onMessageLongPress?: (type: MsgType, index: number, content: string, message: MessagePorpsType) => void
+  /**
+   * 点击头像的回调
+   */
+  pressAvatar?: (isSelf: boolean, targetId: string) => void
+  /**
+   * 	android的导航头高度(加上statusBar高度)
+   */
+  androidHeaderHeight: number
+  /**
+   * 你自己的个人资料
+   */
+  userProfile?: { id: string; avatar: string | number }
+  /**
+   * 显示一个loading
+   */
+  historyLoading?: boolean
+  /**
+   * 下拉获取历史记录的回调
+   */
+  loadHistory?: () => void
+  /**
+   * 自定义渲染消息上方的时间
+   */
+  renderMessageTime?: (time: string) => void
+  /**
+   * 自定义当前聊天背景
+   */
+  renderChatBg?: (bg: number | string) => void
+  /**
+   * 	自定义渲染当被拉黑或者被删除的时候显示的提示性消息
+   */
+  renderErrorMessage?: (messageStatus: 0 | 1 | -1 | -2) => void
+  /**
+   * 自定义最右侧面板数据源
+   */
+  panelSource?: { icon: any; title: string; onPress: (e) => void }[]
+  /**
+   * 自定义渲染每个数据源的内容
+   */
+  renderPanelRow?: any
+  /**
+   * 所有键盘下方面板的高度(表情面板，更多面板)
+   */
+  allPanelHeight?: number
+  /**
+   * 	自定义消息发送失败时显示的图标，默认为红色圆形感叹号
+   */
+  messageErrorIcon?: any
+  /**
+   * 自定义左侧消息的背景色
+   */
+  leftMessageBackground?: string
+  /**
+   * 自定义右侧消息的背景色
+   */
+  rightMessageBackground?: string
+  /**
+   * 	所有面板动画时长
+   */
+  allPanelAnimateDuration?: number
+  // 输入组件属性
+  /**
+   * 自定义最下方的表情图标
+   */
+  emojiIcon?: any
+  /**
+   * 占位符
+   */
+  placeholder?: string
+  /**
+   * 自定义最下方的键盘图标
+   */
+  keyboardIcon?: any
+  /**
+   * 自定义最下方的加号图标
+   */
+  plusIcon?: any
+  /**
+   * 自定义最下方的发送图标
+   */
+  sendIcon?: any
+  /**
+   * 自定义未能发送消息时的发送图标
+   */
+  sendUnableIcon?: any
+  /**
+   * 是否启用表情
+   */
+  useEmoji?: boolean
+  /**
+   * 是否启用更多
+   */
+  usePlus?: boolean
+  /**
+   * 输入框样式
+   */
+  inputStyle?: ViewStyle
+  // 弹出层属性
+  /**
+   * 长按消息是否显示一个弹出层
+   */
+  usePopView?: boolean
+  /**
+   * 弹出层样式
+   */
+  popoverStyle?: ViewStyle
+  /**
+   * 	点击多选之后可以使用此方法改变导航条左上角文字实现关闭多选功能
+   */
+  changeHeaderLeft?: () => void
+  /**
+   * 自定义弹出层的每一个item
+   */
+  setPopItems?: any
+  /**
+   * 自定义底部删除按钮的图标
+   */
+  messageDelIcon?: any
+  /**
+   * 自定义底部删除面板
+   */
+  renderDelPanel?: (isSelect: boolean) => void
+  /**
+   * 自定义消息选中时的图标
+   */
+  messageSelectIcon?: any
+  /**
+   * 自定义渲染消息选中和未选中的样式
+   */
+  renderMessageCheck?: (isSelect: boolean) => void
+  // 语音属性
+  /**
+   * 是否使用语音
+   */
+  useVoice?: boolean
+  /**
+   * 自定义切换到语音，按钮显示的文字
+   */
+  pressInText?: string
+  /**
+   * 自定义按下语音按钮，按钮显示的文字
+   */
+  pressOutText?: string
+  /**
+   * 自定义输入框左侧的语音图标
+   */
+  voiceIcon?: any
+  /**
+   * 自定义左侧语音消息的图标
+   */
+  voiceLeftIcon?: any
+  /**
+   * 自定义右侧语音消息的图标
+   */
+  voiceRightIcon?: any
+  /**
+   * 自定义录音发生错误的图标
+   */
+  voiceErrorIcon?: any
+  /**
+   * 自定义录音发生错误的提示文字
+   */
+  voiceErrorText?: string
+  /**
+   * 自定义取消录音的图标
+   */
+  voiceCancelIcon?: any
+  /**
+   * 自定义取消录音的提示文字
+   */
+  voiceCancelText?: string
+  /**
+   * 自定义按下语音按钮显示的文字
+   */
+  voiceNoteText?: string
+  /**
+   * 自定义按下语音按钮显示的图标
+   */
+  voiceSpeakIcon?: any
+  /**
+   * 语音的存储路径
+   */
+  audioPath?: string
+  /**
+   * 录制语音中的回调
+   */
+  audioOnProgress?: () => void
+  /**
+   * 	录制语音结束的回调
+   */
+  audioOnFinish?: () => void
+  /**
+   * 初始化语音路径的回调
+   */
+  audioInitPath?: () => void
+  /**
+   * 开始录音的回调
+   */
+  audioRecord?: () => void
+  /**
+   * 停止录音的回调
+   */
+  audioStopRecord?: () => void
+  /**
+   * 暂停录音的回调
+   */
+  audioPauseRecord?: () => void
+  /**
+   * 重置录音的回调
+   */
+  audioResumeRecord?: () => void
+  /**
+   * 语音的长度
+   */
+  audioCurrentTime?: number
+  /**
+   * 是否获取到录音的句柄
+   */
+  audioHandle?: boolean
+  /**
+   * 修改是否获取录音句柄的状态
+   */
+  setAudioHandle?: () => void
+  /**
+   * 是否有录音权限
+   */
+  audioHasPermission?: boolean
+  /**
+   * android检查录音权限的回调
+   */
+  requestAndroidPermission?: () => void
+  /**
+   * 检查是否已获得录音权限
+   */
+  checkPermission?: () => void
+  /**
+   * 是否正在加载语音
+   */
+  voiceLoading?: boolean
+  /**
+   * 是否正在播放语音
+   */
+  voicePlaying?: boolean
+  /**
+   * 自定义语音加载时左侧消息颜色
+   */
+  voiceLeftLoadingColor?: string
+  /**
+   * 自定义语音加载时右侧消息颜色
+   */
+  voiceRightLoadingColor?: string
+  // 聊天气泡属性
+  /**
+   * 	自定义渲染文本消息
+   */
+  renderTextMessage?: (data: any) => void
+  /**
+   * 自定义渲染图片消息
+   */
+  renderImageMessage?: (data: any) => void
+  /**
+   * 自定义渲染语音消息
+   */
+  renderVoiceMessage?: (data: any) => void
+  /**
+   * 自定义渲染语音外部容器
+   */
+  renderVoiceView?: (data: any) => void
+  /**
+   * 自定义渲染视频消息
+   */
+  renderVideoMessage?: (data: any) => void
+  /**
+   * 自定义定位消息
+   */
+  renderLocationMessage?: (data: any) => void
+  /**
+   * 自定义分享消息
+   */
+  renderShareMessage?: (data: any) => void
+  /**
+   * 自定义视频聊天信息
+   */
+  renderVideoCallMessage?: (data: any) => void
+  /**
+   * 自定义语音聊天信息
+   */
+  renderVoiceCallMessage?: (data: any) => void
+  /**
+   * 自定义红包信息
+   */
+  renderRedEnvelopeMessage?: (data: any) => void
+  /**
+   * 自定义文件信息
+   */
+  renderFileMessage?: (data: any) => void
+  /**
+   * 自定义系统消息
+   */
+  renderSystemMessage?: (data: any) => void
 }
 
 export class ChatScreen extends PureComponent<ChatScreenPropsType, any> {
   static defaultProps: {}
-  constructor(): void
+  constructor()
   componentDidMount(): void
   componentWillMount(): void
   render(): JSX.Element
